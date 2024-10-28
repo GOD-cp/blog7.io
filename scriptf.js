2216278c75a97b7ad385c730148e2b57af865538
GOD-cp <gp21a008@oecu.jp>
2024-10-29 07:11:10 +0900

    Add files via upload

---
+++ scriptf.js
@@ -0,0 +1,33 @@
+// ×ボタンで画像を消す機能の追加
+document.addEventListener("DOMContentLoaded", function() {
+    const closeButtons = document.querySelectorAll('.close-btn',);
+
+    closeButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const img = button.parentElement.querySelector('img'); // ×ボタンの親要素内の画像を取得
+            img.style.display = 'none'; // ×ボタンが押されたら画像を消す
+            button.style.display = 'none'; // ボタン自体も消す
+             
+        });
+        });
+    });
+    // ×ボタンで画像を消す機能の追加
+document.addEventListener("DOMContentLoaded", function() {
+    const closeButtons = document.querySelectorAll('.close-btnx',);
+
+    closeButtons.forEach(button => {
+        button.addEventListener('click', function() {
+            const img = button.parentElement.querySelector('img'); // ×ボタンの親要素内の画像を取得
+            img.style.display = 'none'; // ×ボタンが押されたら画像を消す
+            button.style.display = 'none'; // ボタン自体も消す
+             
+        });
+        });
+    });
+   
+
+   
+
+
+
+       
